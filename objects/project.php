00347ff2ad58f4688af3c82c6d0d6261f0a955fa
Roller Larry <rollerlarry51197@gmail.com>
2018-04-21 01:37:37 +0700

    delete function create image from here and   move it to class image object

--- objects/project.php
+++ objects/project.php
@@ -7,15 +7,8 @@
 		public $project_id;
 		public $project_name;
 		public $folder_name;
-
-
-		//Image
-		private $table_name_img = "tbimage";
 		public $current_user;
-		public $date_created;	
 
-		
-			
 		public function __construct($db){
 			$this->conn = $db;
 		}
@@ -46,38 +39,7 @@
             $stmt->bindParam(":project_name", $this->project_name);
 	        $stmt->bindParam(":folder_name", $this->folder_name);
 
-	        //Create folder
-    		mkdir('image/'.$this->folder_name);
-
-
-
-    		//Insert image path for sql
-    		date_default_timezone_set('Asia/Ho_Chi_Minh');
-	        $date_created = date('d-m-Y');
-	        $imageDir = '/image/';
-			$image_path=$imageDir.$this->folder_name.'/'.$date_created.'/'.$this->current_user;
-			$query_img = "INSERT INTO
-                    	" . $this->table_name_img . "
-                	SET
-                    imagePath= '{$image_path}', DateCreated= '{$date_created}'";
-            $stmt_img = $this->conn->prepare($query_img);
-
-
-	        //Move image to folder
-	        // Updating ................
-		    // $currentDir = getcwd();
-		    // $uploadDirectory = "/image/";
-
-		    // $folderName = $this->folder_name ."/";
-		    // $dateCreated = $this->date_created ."/";
-
-		    
-
-		    //$uploadPath = $currentDir.$uploadDirectory .$folderName .$dateCreated . basename($fileName); 
-
-		    
-
-	        if($stmt->execute() && $stmt_img->execute()){
+	        if($stmt->execute()){
             return true;
 	        }
 	     
@@ -119,9 +81,8 @@
 
 	        // prepare query
 	        $stmt_del = $this->conn->prepare($query_del);
-
-
 	        $stmt_sl = $this->conn->prepare($query_sl);
+
 		    $stmt_sl->execute();
 		    $row = $stmt_sl->fetch(PDO::FETCH_ASSOC);
 
