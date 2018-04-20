4d377280e5259a92a471a63e3957f524f1879b98
Roller Larry <rollerlarry51197@gmail.com>
2018-04-21 01:40:01 +0700

    edit function update project

--- project/update.php
+++ project/update.php
@@ -9,6 +9,7 @@
 	// include database and object files
 	include_once '../config/database.php';
 	include_once '../objects/project.php';
+	include_once '../objects/image.php';
 	 
 	// get database connection
 	$database = new Database();
@@ -16,16 +17,24 @@
 	 
 	$project = new Project($db);
 	
-	if(isset($_GET['projectid']) || isset($_GET['projectname']) || isset($_GET['projectfolder'])){
+	if(isset($_GET['projectid']) || isset($_GET['projectname']) || isset($_GET['foldername']) || isset($_GET['currentuser'])){
 		$project_id = $_GET['projectid'];
 		$project_name = $_GET['projectname'];
 		$folder_name = $_GET['foldername'];
+		$current_user = $_GET['currentuser'];
 	}  
 	 
 	$project->project_id = $project_id;
 	 
 	$project->project_name = $project_name;
 	$project->folder_name = $folder_name;
+	$project->current_user = $current_user;
+
+
+	$image = new Image($db);
+	
+	$image->current_user = $current_user;
+	$image->folder_name = $folder_name;
 	 
 	// update the product
 	if($project->update()){
