e88c8d23a150de234c325c8f3e6476b6d67d23cb
Roller Larry <rollerlarry51197@gmail.com>
2018-04-21 01:35:40 +0700

    update function create folder, subfolder and move image to this folder when create project

--- objects/image.php
+++ objects/image.php
@@ -3,9 +3,8 @@
 		private $conn;
 		private $table_name = "tbimage";
 
-		public $image_id;
-		public $image_path;
-		public $date_created;
+		public $current_user;
+		public $folder_name;
 
 		public function __construct($db){
 			$this->conn = $db;
@@ -23,14 +22,160 @@
 		    return $stmt;
 		}
 
+		//Create image
+		function create(){
 
-		function update(){
-		
-		    if($stmt_img->execute()){
+			if (isset($_POST['btnUpload']))
+			{
+			$url = "http://localhost/ProjectPHP-API/project/create.php"; // request URL
+			$filename = $_FILES['file']['name'];
+			$filedata = $_FILES['file']['tmp_name'];
+			$filesize = $_FILES['file']['size'];
+
+				if ($filedata != '')
+				{
+				    $headers = array("Content-Type:multipart/form-data"); // cURL headers for file uploading
+				    $postfields = array("filedata" => "@$filedata", "filename" => $filename);
+				    $ch = curl_init();
+				    $options = array(
+				        CURLOPT_URL => $url,
+				        CURLOPT_HEADER => true,
+				        CURLOPT_POST => 1,
+				        CURLOPT_HTTPHEADER => $headers,
+				        CURLOPT_POSTFIELDS => $postfields,
+				        CURLOPT_INFILESIZE => $filesize,
+				        CURLOPT_RETURNTRANSFER => true
+				    ); // cURL options
+				    curl_setopt_array($ch, $options);
+				    curl_exec($ch);
+				    if(!curl_errno($ch))
+				    {
+				        $info = curl_getinfo($ch);
+				        if ($info['http_code'] == 200)
+				            $errmsg = 1; //"File uploaded successfully"
+				    }
+				    else
+				    {
+				        $errmsg = curl_error($ch);
+				    }
+				    curl_close($ch);
+				}
+				else
+				{
+				    $errmsg = 0; //"Please select the file"
+				}
+
+				//Insert image path to sql
+	    		date_default_timezone_set('Asia/Ho_Chi_Minh');
+		        $date_created = date('d-m-Y');
+		        $image_path = 'image/'.$this->folder_name.'/'.$date_created.'/'.$this->current_user.'/'.$filename;
+				$query = "INSERT INTO
+	                    	" . $this->table_name . "
+	                	SET
+	                    imagePath= '{$image_path}', DateCreated= '{$date_created}'";
+
+	            $stmt = $this->conn->prepare($query);
+
+	            //Create folder
+		 	   if (!file_exists('image/'.$this->folder_name)) {
+				    mkdir('image/'.$this->folder_name, 0777, true);
+					mkdir('image/'.$this->folder_name.'/'.$date_created, 0777, true);
+					mkdir('image/'.$this->folder_name.'/'.$date_created.'/'.$this->current_user, 0777, true);
+				}
+
+	            //Move image to folder
+	            if ($filedata != '' && $filename != ''){
+				 	move_uploaded_file($filedata,$image_path);
+	            }
+			    
+
+			}
+
+            if($stmt->execute()){
             return true;
 	        }
 	     
 	        return false;
+		}
+
+
+		function update(){
+
+			// if (isset($_POST['btnUpload']))
+			// {
+			// $url = "http://localhost/ProjectPHP-API/project/update.php"; // request URL
+			// $filename = $_FILES['file']['name'];
+			// $filedata = $_FILES['file']['tmp_name'];
+			// $filesize = $_FILES['file']['size'];
+
+			// 	if ($filedata != '')
+			// 	{
+			// 	    $headers = array("Content-Type:multipart/form-data"); // cURL headers for file uploading
+			// 	    $postfields = array("filedata" => "@$filedata", "filename" => $filename);
+			// 	    $ch = curl_init();
+			// 	    $options = array(
+			// 	        CURLOPT_URL => $url,
+			// 	        CURLOPT_HEADER => true,
+			// 	        CURLOPT_POST => 1,
+			// 	        CURLOPT_HTTPHEADER => $headers,
+			// 	        CURLOPT_POSTFIELDS => $postfields,
+			// 	        CURLOPT_INFILESIZE => $filesize,
+			// 	        CURLOPT_RETURNTRANSFER => true
+			// 	    ); // cURL options
+			// 	    curl_setopt_array($ch, $options);
+			// 	    curl_exec($ch);
+			// 	    if(!curl_errno($ch))
+			// 	    {
+			// 	        $info = curl_getinfo($ch);
+			// 	        if ($info['http_code'] == 200)
+			// 	            $errmsg = 1; //"File uploaded successfully"
+			// 	    }
+			// 	    else
+			// 	    {
+			// 	        $errmsg = curl_error($ch);
+			// 	    }
+			// 	    curl_close($ch);
+			// 	}
+			// 	else
+			// 	{
+			// 	    $errmsg = 0; //"Please select the file"
+			// 	}
+
+			// 	//Insert image path to sql
+	  //   		date_default_timezone_set('Asia/Ho_Chi_Minh');
+		 //        $date_created = date('d-m-Y');
+		 //        $image_path = 'image/'.$this->folder_name.'/'.$date_created.'/'.$this->current_user.'/'.$filename;
+			// 	$query = "UPDATE
+	  //                   	" . $this->table_name . "
+	  //               	SET
+	  //                   imagePath= '{$image_path}', DateCreated= '{$date_created}'
+
+			// 			WHERE
+							
+	  //                   ";
+
+	  //           $stmt = $this->conn->prepare($query);
+
+	  //           //Create folder
+			//     if (!file_exists('image/'.$this->folder_name)) {
+			// 	    mkdir('image/'.$this->folder_name, 0777, true);
+			// 		mkdir('image/'.$this->folder_name.'/'.$date_created, 0777, true);
+			// 		mkdir('image/'.$this->folder_name.'/'.$date_created.'/'.$this->current_user, 0777, true);
+			// 	}
+
+	  //           //Move image to folder
+	  //           if ($filedata != '' && $filename != ''){
+			// 	 	move_uploaded_file($filedata,$image_path);
+	  //           }
+			    
+
+			// }
+		
+		 //    if($stmt->execute()){
+   //          return true;
+	  //       }
+	     
+	  //       return false;
 
 		}
 
